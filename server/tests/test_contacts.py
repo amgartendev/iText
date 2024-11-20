fd30751995458f3b80551263439437b7d28071b5
amgartendev <joaoamgarten2003@gmail.com>
2024-11-20 20:55:39 +0000

    add type hints and return types for all test functions

--- server/tests/test_contacts.py
+++ server/tests/test_contacts.py
@@ -8,6 +8,8 @@ import sys
 
 sys.path.insert(1, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from typing import Type
+
 import pytest
 import requests
 from core.configs import settings
@@ -19,14 +21,14 @@ client = TestClient(app)  # TODO Use the client to make internal requests instea
 
 
 @pytest.mark.parametrize("user_uid, expected_status, expected_type", [("2fbc5d3b-6ed4-4cc6-9976-c22b355c4316", 200, list), ("h1oi2hdj", 404, dict)])
-def test_get_contacts(user_uid, expected_status, expected_type):
+def test_get_contacts(user_uid: str, expected_status: int, expected_type: Type) -> None:
     response = requests.get(f"{ENDPOINT}/{user_uid}")
     assert response.status_code == expected_status
     assert isinstance(response.json(), expected_type)
 
 
 @pytest.mark.parametrize("user_uid, contact_uid, contact_name, expected_status", [("2fbc5d3b-6ed4-4cc6-9976-c22b355c4316", "d58e8a2e-b60f-429c-a8db-19b30a4a1c31", "PyTest Contact", 201), ("2fbc5d3b-6ed4-4cc6-9976-c22b355c4316", "d58e8a2e-b60f-429c-a8db-19b30a4a1c31", "PyTest Contact", 406), ("h1oi2hdj", "d58e8a2e-b60f-429c-a8db-19b30a4a1c31", "PyTest Contact", 404), ("d58e8a2e-b60f-429c-a8db-19b30a4a1c31", "h1oi2hdj", "PyTest Contact", 404)])
-def test_post_create_contact(user_uid, contact_uid, contact_name, expected_status):
+def test_post_create_contact(user_uid: str, contact_uid: str, contact_name: str, expected_status: int) -> None:
     payload = {
         "user_uid": user_uid,
         "user_added": contact_uid,
@@ -37,6 +39,6 @@ def test_post_create_contact(user_uid, contact_uid, contact_name, expected_statu
 
 
 @pytest.mark.parametrize("user_uid, contact_uid, expected_status", [("2fbc5d3b-6ed4-4cc6-9976-c22b355c4316", "d58e8a2e-b60f-429c-a8db-19b30a4a1c31", 200), ("2fbc5d3b-6ed4-4cc6-9976-c22b355c4316", "h1oi2hdj", 404)])
-def test_get_contact_info(user_uid, contact_uid, expected_status):
+def test_get_contact_info(user_uid: str, contact_uid: str, expected_status: int):
     response = requests.get(f"{ENDPOINT}/info/{user_uid}/{contact_uid}")
     assert response.status_code == expected_status
--- server/tests/test_users.py
+++ server/tests/test_users.py
@@ -19,20 +19,20 @@ test_username = "jamgarten"
 client = TestClient(app)  # TODO Use the client to make internal requests instead of using the requests library
 
 
-def test_get_users():
+def test_get_users() -> None:
     response = requests.get(f"{ENDPOINT}/")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
 
 
 @pytest.mark.parametrize("user_uid, expected_status", [("2fbc5d3b-6ed4-4cc6-9976-c22b355c4316", 200), ("h1oi2hdj", 404)])
-def test_get_user(user_uid, expected_status):
+def test_get_user(user_uid: str, expected_status: int) -> None:
     response = requests.get(f"{ENDPOINT}/{user_uid}")
     assert response.status_code == expected_status
 
 
 @pytest.mark.parametrize("user_uid, expected_status", [("2fbc5d3b-6ed4-4cc6-9976-c22b355c4316", 202), ("d58e8a2e-b60f-429c-a8db-19b30a4a1c31", 406)])
-def test_update_user(user_uid, expected_status):
+def test_update_user(user_uid: str, expected_status: int) -> None:
     payload = {
         "first_name": "João",
         "last_name": "Amgarten",
@@ -44,13 +44,13 @@ def test_update_user(user_uid, expected_status):
 
 
 @pytest.mark.parametrize("user_uid, expected_status", [("2fbc5d3b-6ed4-4cc6-9976-c22b355c4316", 204), ("d58e8a2e-b60f-429c-a8db-19b30a4a1c31", 204), ("h1oi2hdj", 404)])
-def test_update_deleted_flag(user_uid, expected_status):
+def test_update_deleted_flag(user_uid: str, expected_status: int) -> None:
     response = requests.put(f"{ENDPOINT}/delete/{user_uid}")
     assert response.status_code == expected_status
 
 
 @pytest.mark.parametrize("username, password, expected_status", [(test_username, "123456", 200), (test_username, "123", 401), ("random_username", "123456", 401)])
-def test_login(username, password, expected_status):
+def test_login(username: str, password: str, expected_status: int):
     payload = {
         "username": username,
         "password": password
